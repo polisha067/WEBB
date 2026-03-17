3c1848db36c8b8108a163526931b16311be24d16
drra1nee <semen.sabetov@yandex.ru>
2026-03-17 16:45:06 +0300

    upd models

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Subscription
+from .models import Subscription, UserSubscription
+
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -7,3 +8,11 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_filter = ['is_active', 'duration_days']
     search_fields = ['name', 'features']
     readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subscription', 'purchased_at', 'expires_at', 'status', 'is_active']
+    list_filter = ['status', 'is_active', 'purchased_at']
+    search_fields = ['user__username', 'subscription__name']
+    readonly_fields = ['purchased_at', 'expires_at']
