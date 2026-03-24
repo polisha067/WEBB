fb0fd0a0f181edeb565ce70a26dd0aa6fa252a67
Daria <13.daria.novikova@gmail.com>
2026-03-24 21:53:33 +0300

    NOW added API viewsets for subscriptions

---
+++ subscriptions/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import Subscription, UserSubscription
+from .serializers import SubscriptionSerializer, UserSubscriptionSerializer
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    """API для тарифных планов"""
+    queryset = Subscription.objects.filter(is_active=True)
+    serializer_class = SubscriptionSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class UserSubscriptionViewSet(viewsets.ModelViewSet):
+    """API для подписок пользователей"""
+    serializer_class = UserSubscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return UserSubscription.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
