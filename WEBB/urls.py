806035792f2424f07bfdb630ce2771d61cba40ac
polisha <polinazuravlik2@gmail.com>
2026-03-27 12:49:32 +0300

    urls путь

--- WEBB/urls.py
+++ WEBB/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/movies/', include('movies.urls')),
-    #path('api/watchlist/', include('watchlist.urls')),
+    path('api/watchlist/', include('watchlist.urls')),
     path('api/subscriptions/', include('subscriptions.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
